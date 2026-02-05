a0baafc1b533b90fe721b504e7f6773f406174b6
VANZZ_STORE <novanardi13@gmail.com>
2026-02-06 06:48:02 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,19 @@
+if (localStorage.getItem("myNumber")) {
+    window.location.href = "chat.html"; 
+}
+
 const SUPABASE_URL = "https://qxxmxzxipoodeobehcoy.supabase.co";
 const SUPABASE_KEY = "sb_publishable_53x04KOXjY6Eg9tfhAPQWg_MSfaBSPV";
-
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 async function login() {
   const number = document.getElementById("number").value.trim();
   const pin = document.getElementById("pin").value.trim();
+
+  if (!number || !pin) {
+    alert("Isi nomor dan PIN!");
+    return;
+  }
 
   const { data, error } = await db
     .from("numbers")
@@ -21,5 +29,6 @@ async function login() {
 
   localStorage.setItem("myNumber", data.number);
   localStorage.setItem("myName", data.owner);
+  
   window.location.href = "chat.html";
 }
